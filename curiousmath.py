37489e9130eac369d733a710a6238f1108c66b9a
Carson <No>
2026-09-20 16:47:49 -0500

    Better SNR detection of multiple frequencies

--- curiousmath.py
+++ curiousmath.py
@@ -6,9 +6,11 @@ import matplotlib.pyplot as plt
 
 # Assume pulse occurs at t=0; Stand in for either detecting a frequency shift or inital pulse
 
+# NOTE Lower frequencies have a longer detection time thus reduce resolution; Higher frequencies are better
+
 SIMULAITON_LENGTH = 5 # s
 SPEED_OF_SOUND = 1 # 343 #m/s
-SAMPLE_RATE_ADC = 9600 # Ardunio using analogRead() but can use hax to icnrease it ot 38.5kHz or buy/user better ADCs
+SAMPLE_RATE_ADC = 9600 # NOTE Ardunio using analogRead() but can use hax to icnrease it ot 38.5kHz or buy/user better ADCs
 MIC_SAMPLES = int(SIMULAITON_LENGTH*SAMPLE_RATE_ADC)
 simulation_times = np.linspace(0, SIMULAITON_LENGTH, num=MIC_SAMPLES)
 
@@ -41,23 +43,24 @@ class Mic:
         results[sampleStart:sampleStart+length] = pulse_signal[0:length]
 
         # Noise
-        results = results + np.random.normal(0, 0.01, MIC_SAMPLES) # TODO Noise intolerance
+        results = results + np.random.normal(0, 0.01, MIC_SAMPLES)
 
         return results
 
-NUM_MICS = 6
 Mic([1,0,0])
 Mic([-1,0,0])
 Mic([0,1,0])
 Mic([0,-1,0])
 Mic([0,0,1])
 Mic([0,0,-1])
+NUM_MICS = len(micList)
 
 samples = [m.sampleList() for m in micList]
 
 # fMax = fS/2; deltaF (Resolution) = fs/N = 1/T
 MAXIMUMMEASUREABLEFREQUENCY = SAMPLE_RATE_ADC / 2 # Nyquilst criteria
 LOWESTMEASURABLEFREQUENCY = 200 # Sets frame window
+SNRTHRESHOLD = 25 # dB
 
 SamplesPerWindow = int(np.ceil(5 * SAMPLE_RATE_ADC / LOWESTMEASURABLEFREQUENCY)) # 5 cycles of lowest frequency set max number of samples needed for detection
 SampleWindowPeriod = SamplesPerWindow/SAMPLE_RATE_ADC # NOTE: This also determines the minimum transmission pulse length
@@ -68,7 +71,7 @@ sampleWindow = np.zeros((NUM_MICS, SamplesPerWindow))
 freqs = np.fft.rfftfreq(SamplesPerWindow, d=(1/SAMPLE_RATE_ADC)) # Get bins; Samples, timestep
 
 frequenciesVsTime = np.zeros((MIC_SAMPLES, NUM_MICS, len(freqs))) # Spectrum of each mic per timestep
-detectedPeakFrequenciesVsTime = np.zeros((MIC_SAMPLES, NUM_MICS)) # Peak Frequency (#NOTE IRL may need to look specifcally at broadcast frequencies (discrete bandpass) to ensure interference)
+detectedFrequenciesVsTime = np.zeros((MIC_SAMPLES, NUM_MICS, len(freqs))) # Peak Frequency (#NOTE IRL may need to look specifcally at broadcast frequencies (discrete bandpass) to ensure interference)
 
 # Detection Loop (What runs on the hardware)
 for i, t in enumerate(simulation_times):
@@ -78,10 +81,11 @@ for i, t in enumerate(simulation_times):
         sampleWindow[j, -1] = sampleArr[i]
 
     freqMag = np.fft.rfft(sampleWindow, axis=1)
-    frequenciesVsTime[i] = data = 20*np.log10(np.abs(freqMag) / SamplesPerWindow)
+    frequenciesVsTime[i] = data = 20*np.log10(np.maximum(np.abs(freqMag), 1e-12) / SamplesPerWindow)
     noise = np.median(data, axis=1).reshape((data.shape[0],1))
-    SNR = data - noise
 
+    SNR = data - noise
+    detectedFrequenciesVsTime[i] = np.where(SNR > SNRTHRESHOLD, SNR, 0)
 
 
 
@@ -97,7 +101,7 @@ sub.set_title("Pulse Signal")
 sub = figPulse.add_subplot(2, 1, 2)
 freqsPulse = np.fft.fftfreq(PULSE_SAMPLES, d=(1/SAMPLE_RATE_ADC)) # Get bins; Samples, timestep
 mags = np.fft.fft(pulse_signal)
-sub.plot(freqsPulse, mags)
+sub.plot(freqsPulse, np.abs(mags))
 sub.set_xlabel("Frequency")
 sub.set_ylabel("Amplitude")
 sub.set_title("FFT of Pulse")
@@ -114,9 +118,7 @@ sub.set_ylabel("Amplitude")
 
 sub = figRecSignals.add_subplot(3, 1, 2)
 for i in range(NUM_MICS):
-    noise = np.median(frequenciesVsTime[:, i], axis=1).reshape((frequenciesVsTime[:, i].shape[0],1))
-    SNR = frequenciesVsTime[:, i]-noise 
-    data = np.where(SNR > 25, frequenciesVsTime[:, i], 0) # NOTE There are some peaks caused by frequency games around the 20dB
+    data = detectedFrequenciesVsTime[:, i]
     data = freqs[np.argmax(data, axis=1)]
 
 
@@ -124,20 +126,15 @@ for i in range(NUM_MICS):
 
 sub.set_xlabel("Simulation Time")
 sub.set_ylabel("Frequency")
-sub.set_title("Peak Frequency vs Time of Microphones")
+sub.set_title("Peak SNR vs Time of Microphones")
 
 sub = figRecSignals.add_subplot(3, 1, 3)
 for i in range(NUM_MICS):
-    peak = np.max(frequenciesVsTime[:, i], axis=1)
-    noise = np.median(frequenciesVsTime[:, i], axis=1)
-
-    ratio = peak - noise
-
-    sub.plot(simulation_times, ratio, color=colors[i])
+    sub.plot(simulation_times, frequenciesVsTime[:, i], color=colors[i])
 
 sub.set_xlabel("Simulation Time")
-sub.set_ylabel("Peak Ratio (dB)")
-sub.set_title("Peak Ratio of Mics vs Time")
+sub.set_ylabel("Amplitude (dB)")
+sub.set_title("Amplitude of Mics vs Time")
 
 figRecSignals.tight_layout()
 
@@ -148,9 +145,7 @@ sub = figFTMic1.add_subplot(1, 1, 1, projection='3d')
 mask = (freqs >= 0) & (freqs <= 1000)
 T, F = np.meshgrid(simulation_times ,freqs[mask], indexing='ij')
 
-noise = np.median(frequenciesVsTime[:, 1, mask], axis=1).reshape((frequenciesVsTime[:, 1, mask].shape[0],1))
-sub.plot_surface(T, F, frequenciesVsTime[:, 1, mask]-noise)
-# sub.set_ylim(0, 1000)
+sub.plot_surface(T, F, frequenciesVsTime[:, 1, mask])
 
 figFTMic1.tight_layout()
 
